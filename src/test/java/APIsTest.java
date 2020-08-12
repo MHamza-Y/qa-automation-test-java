6fe93698e1e904e2067985e5a21fb0a53e6e78a1
MHamza-Y <m.hamza.yousaf.22@gmail.com>
2020-08-12 12:26:23 +0500

    Added API test

--- src/test/java/APIsTest.java
+++ src/test/java/APIsTest.java
@@ -1,8 +1,86 @@
+import com.google.errorprone.annotations.Var;
+import io.restassured.internal.RestAssuredResponseImpl;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.StringUtils;
 
-public class APIsTest extends BaseTests{
-    @Test (dataProvider = "input-sheet")
-    public void APIsRequestTest() {
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import static io.restassured.RestAssured.given;
+
+public class APIsTest extends BaseTests {
+    @Test(dataProvider = "input-sheet")
+    public void APIsRequestTest(Map<String, String> sheet) {
+        RequestSpecification specification ;
+        Response response = new RestAssuredResponseImpl();
+        String payloadStr = sheet.get("Request Payload (json)");
+        String responseStr = sheet.get("Response Contains");
+        String method = sheet.get("Method");
+        String api = sheet.get("API");
+
+        if (payloadStr.equals("N/A")){
+            payloadStr = "";
+        }
+        if (responseStr.equals("N/A")) {
+            responseStr = "";
+        }
+
+        specification = given().body(payloadStr).when();
+        if (method.equals("GET")) {
+            response = specification.get(api);
+
+        } else if (method.equals("POST")) {
+            response = specification.post(api);
+        }
+
+       /* Assert.assertEquals(
+                response.statusCode(),
+                Integer.parseInt(sheet.get("Expected Response Code")),
+                "Response status code do not match"
+        ); *///assert status code
+
+        Assert.assertTrue(
+                Integer.parseInt(response.header("Content-Length"))
+                        <=
+                        Integer.parseInt(sheet.get("Expected Response Size in kilobyte (less than)"))*1024,
+                "Response size greater than expected"
+                ); //assert size
+
+        Assert.assertTrue(
+                response.timeIn(TimeUnit.SECONDS)
+                        <=
+                        Integer.parseInt(sheet.get("Expected Response time in seconds (less than)")),
+                "Response time greater than expected"
+        ); // assert response time
+
+        Assert.assertTrue(
+                responseContainsExpectedKeys(
+                        response.asString(),
+                        sheet.get("Response Contains")
+                ),
+                "Response does not contain expected keys"
+        );
+        System.out.println(payloadStr);
+        System.out.println(response.header("Content-Length"));
+        System.out.println(response.headers());
+        System.out.println(response.asString());
+    }
+
+    private boolean responseContainsExpectedKeys(String responseBody, String keysArrayAsString) {
+        List<String> expectedStrList = StringUtils.parseStringToList(keysArrayAsString);
+        for(String item:expectedStrList) {
+            if(item.equals("N/A")) {
+                return true;
+            }
+            if(!responseBody.contains(item)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
